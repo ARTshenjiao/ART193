e5315f6346e761dd22b66d6623ce19fa6badf9ce
angryjinyan <angryjinyan@gmail.com>
2016-09-15 17:56:05 -0500

    Add files via upload

---
+++ PS2.c
@@ -0,0 +1,46 @@
+#define BOY 	0
+#define GIRL 	1
+
+typedef struct{
+	spinlock_t lock;
+	int count[2];			// count[1] girl, count[0] boy
+} boygirl_lock_t;
+
+void boygirl_lock_init(boygirl_lock_t *lock){
+	spin_lock_init(lock->lock);
+	lock->count[0] =0;
+	lock->count[1] =0;
+}
+void boygirl_lock(boygirl_lock_t *lock,int gender){
+	while(1){
+		spin_lock_irq(&(lock->lock));
+		if(gender==0 && lock->count[1] == 0){
+			count[0]++;
+			break;
+		}
+		if(gender == 1 && lock->count[0] == 0){
+			count[1]++;
+			break;
+		}
+		spin_unlock_irq(&(lock->lock));
+	}
+	spin_unlock_irq(&(lock->lock));
+	return;
+}
+
+void boygirl_unlock(boygirl_lock_t *lock,int gender){
+	while(1){
+		spin_lock_irq(&(lock->lock));
+		if(gender==0 && lock->count[0] > 0){
+			count[0]--;
+			break;
+		}
+		if(gender == 1 && lock->count[1] > 0){
+			count[1]--;
+			break;
+		}
+		spin_unlock_irq(&(lock->lock));
+	}
+	spin_unlock_irq(&(lock->lock));
+	return;
+}
